0234cc388df938feb9d2c753206cd3260dcf8405
元一 <xuhj2015@foxmail.com>
2021-08-12 16:46:07 +0800

    Update update_pg_info.sh

--- data_imp/update_pg_info.sh
+++ data_imp/update_pg_info.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sed -i '29d' project.mml
-sed -i '29a\    host: "10.10.10.7"' project.mml
+sed -i '29a\    host: "172.16.1.191"' project.mml
 sed -i '29a\    port: "5432"' project.mml
 sed -i '29a\    user: "postgres"' project.mml
-sed -i '29a\    password: "123456"' project.mml
+sed -i '29a\    password: "botech123"' project.mml
 sed -i '29a\    dbname: "gis"' project.mml
+
+sed -i '103,111d' scripts/get-external-data.py
